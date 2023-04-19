4d0df8239fe7ddda33e95c568d70c1bc6dd79262
ivaneev-ivan <ivaneev.ivane@gmail.com>
2023-04-19 19:38:37 +0300

    resize window in all point

--- src/pages/content/components/Messages/App.tsx
+++ src/pages/content/components/Messages/App.tsx
@@ -18,6 +18,24 @@ const App = () => {
 
       if (resizeDirection === "se") {
         setSize({ width: newX - position.x, height: newY - position.y })
+      } else if (resizeDirection === "sw") {
+        setSize({
+          width: position.x - newX + size.width,
+          height: newY - position.y,
+        })
+        setPosition({ x: newX, y: position.y })
+      } else if (resizeDirection === "ne") {
+        setSize({
+          width: newX - position.x,
+          height: position.y - newY + size.height,
+        })
+        setPosition({ x: position.x, y: newY })
+      } else if (resizeDirection === "nw") {
+        setSize({
+          width: position.x - newX + size.width,
+          height: position.y - newY + size.height,
+        })
+        setPosition({ x: newX, y: newY })
       }
     }
 
@@ -35,23 +53,38 @@ const App = () => {
       document.removeEventListener("mousemove", handleMouseMove)
       document.removeEventListener("mouseup", handleMouseUp)
     }
-  }, [isDragging, resizeDirection, position])
+  }, [isDragging, resizeDirection, position, size])
 
   const handleMouseDown = (event) => {
     const startX = event.clientX
     const startY = event.clientY
     const offsetX = startX - position.x
     const offsetY = startY - position.y
-    const offsetWidth = startX - size.width
-    const offsetHeight = startY - size.height
 
     if (
       startX > position.x + size.width - 10 &&
       startY > position.y + size.height - 10
     ) {
       setResizeDirection("se")
+    } else if (
+      startX < position.x + 10 &&
+      startY > position.y + size.height - 10
+    ) {
+      setResizeDirection("sw")
+    } else if (
+      startX > position.x + size.width - 10 &&
+      startY < position.y + 10
+    ) {
+      setResizeDirection("ne")
+    } else if (startX < position.x + 10 && startY < position.y + 10) {
+      setResizeDirection("nw")
     } else {
       setIsDragging(true)
+    }
+
+    const handleMouseUp = () => {
+      setIsDragging(false)
+      setResizeDirection("")
     }
 
     const handleMouseMove = (event) => {
@@ -65,14 +98,30 @@ const App = () => {
 
       if (resizeDirection === "se") {
         setSize({ width: newWidth, height: newHeight })
-      }
-    }
+      } else if (resizeDirection === "sw") {
+        setSize({
+          width: position.x - event.clientX + size.width,
+          height: newHeight,
+        })
 
-    const handleMouseUp = () => {
-      setIsDragging(false)
-      setResizeDirection(null)
-      document.removeEventListener("mousemove", handleMouseMove)
-      document.removeEventListener("mouseup", handleMouseUp)
+        setSize({
+          width: position.x - event.clientX + size.width,
+          height: newHeight,
+        })
+        setPosition({ x: event.clientX - offsetX, y: event.clientY - offsetY })
+      } else if (resizeDirection === "ne") {
+        setSize({
+          width: newWidth,
+          height: position.y - event.clientY + size.height,
+        })
+        setPosition({ x: event.clientX - offsetX, y: event.clientY })
+      } else if (resizeDirection === "nw") {
+        setSize({
+          width: position.x - event.clientX + size.width,
+          height: position.y - event.clientY + size.height,
+        })
+        setPosition({ x: event.clientX - offsetX, y: event.clientY })
+      }
     }
 
     document.addEventListener("mousemove", handleMouseMove)
@@ -82,19 +131,23 @@ const App = () => {
   return (
     <div
       style={{
+        width: size.width + "px",
+        height: size.height + "px",
+        border: "1px solid black",
         position: "fixed",
-        left: position.x,
-        top: position.y,
-        width: size.width,
-        height: size.height,
-        border: "1px solid #000",
+        left: position.x + "px",
+        top: position.y + "px",
+        resize: "both",
+        overflow: "auto",
         background: "lightgrey",
         userSelect: "none",
         zIndex: 9999,
         cursor: resizeDirection ? "se-resize" : "move",
       }}
       onMouseDown={handleMouseDown}
-    ></div>
+    >
+      Размер изменяемого блока
+    </div>
   )
 }
 
